c7515d74741b2fdfe8ac8d5c0115e9190e67750d
root <root@ip-172-31-18-140.ap-northeast-2.compute.internal>
2016-03-30 15:21:53 +0000

    .

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+module.exports = function(app){
+	app.use('/',router);
+};
+router.get('/', function(req, res, next) {	
+	res.send('hello world');
+});
